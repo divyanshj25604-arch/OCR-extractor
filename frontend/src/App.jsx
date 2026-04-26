59991957a41938f48f31ca7bcf645383111bc8a9
Divyansh Jha <divyansh.j25604@nst.rishihood.edu.in>
2026-04-26 19:08:40 +0530

    added styling and connected backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,37 @@
 import React from 'react'
+import './css/app.css'
 import { useState, useEffect } from 'react'
+import copyIcon from './assets/copy-document.png'
+import jsPDF from "jspdf"
 
 const App = () => {
   const [image, setImage] = useState(null)
   const [preview, setPreview] = useState(null)
   const [text, setText] = useState('')
   const [loading, setLoading] = useState(false)
+  const [copied, setCopied] = useState(false)
+
+  const handleCopy = async () => {
+    await navigator.clipboard.writeText(text)
+    setCopied(true)
+    setTimeout(() => setCopied(false), 1500)
+  }
+
+  const handleExport = () => {
+    const doc = new jsPDF()
+
+    const pageWidth = doc.internal.pageSize.getWidth()
+    const margin = 10
+
+    const lines = doc.splitTextToSize(text, pageWidth - margin * 2)
+
+    doc.text(lines, margin, 10)
+    doc.save("extracted-text.pdf")
+  }
 
   return (
     <div>
-      <header>
+      <header className='head'>
         <h1>Extracto</h1>
       </header>
       <div className="action-bar">
@@ -36,7 +58,7 @@ const App = () => {
           setLoading(true)
           const formData = new FormData()
           formData.append('file', image)
-          let response = await fetch("http://127.0.0.1:8000/extract", {
+          fetch(`${import.meta.env.VITE_API_URL}/extract`, {
             method: "POST",
             body: formData
           })
@@ -48,15 +70,22 @@ const App = () => {
           {loading ? 'Processing...' : 'Extract Text'}
         </button>
       </div>
-      <div className="left">
+      <div className="panel">
         <div className="preview">
-          {preview && <img src={preview} alt='preview' className='preview' />}
+          {preview && <img src={preview} alt='preview' className='preview-image' />}
         </div>
-      </div>
-      <div className="right">
         <div className="extracted-text">
           <h2>Extracted Text:</h2>
           <pre>{text}</pre>
+          <div className="text-actions">
+            <button className='copy-btn' onClick={handleCopy} disabled={!text}>
+              <img src={copyIcon} alt='copy' className='copy-icon' />
+              {copied ? "Copied!" : "Copy"}
+            </button>
+            <button className="export" onClick={handleExport}>
+              Export
+            </button>
+          </div>
         </div>
       </div>
     </div>
